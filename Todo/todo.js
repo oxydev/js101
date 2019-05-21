81c3137fcc8f8b093c8e40b494dd50b4e25294c3
Shrijal <tamrakarshrijal@gmail.com>
2019-05-21 19:32:20 +0545

    todo

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,43 +1,92 @@
-var task = document.querySelector('.todo-item');
+// var task = document.querySelector('.todo-item'); 
 var tableBody = document.querySelector('tbody');
 var add = document.querySelector('#add');
 var currentTime = document.querySelector('#current-time');
+var toggleStatusElement = document.querySelector('#status');
 
-/*
-function generateTodo(sn, title, date, status) {
-    var row = document.createElement('tr');
-    row.innerHTML=`<td>${sn}</td>
-                    <td>${title}</td>
-                    <td>${date}</td>
-                    <td>${status}</td>
-                    <td>
-                    <button class="delete">Delete<button><button>complete</button>
-                    </td>
-                    `;
-    tableBody.append(row);
 
-    var button = document.querySelector('.button');
-    button.addEventListener('click',()=>{
-        alert('delete');
-    })
+function toggleStatus(element) {
+    var text = element.innerText;
+    var textList = text.split(" ");
+    //Getting the vale of the text and toggling hide 
+    if(textList[0].toLowerCase() === "show"){
+        element.innerText = 'Hide ' + textList[1];
+    }else{
+        element.innerText = 'Show ' + textList[1];
+    }
+} 
+
+function toggleCompletion(element) {
+    var text = element.innerText;
+    console.log(element.parentElement.previousSibling)
+    //getting the value of the text and toggling hide
+    if(text.trim().toLowerCase() === 'incomplete'){
+        element.innerText = 'Complete';    
+        element.parentElement.previousSibling.innerText = 'InComplete';
+    }else{
+        element.innerText = 'Incomplete';
+        element.parentElement.previousSibling.innerText = 'Complete';
+    }
 }
-*/
+
+toggleStatusElement.addEventListener('click', (e)=>{
+    var clickedElement = e.srcElement;
+    toggleStatus(clickedElement);
+    tableBody.classList.toggle('visible-all');
+});
+
+function toggleTask(e) {
+    e.srcElement.parentElement.parentElement.classList.toggle('hide');
+    //Toggle complete || incomplete
+    toggleCompletion(e.srcElement);
+}
 
 function deleteAction(e) {
     e.srcElement.parentElement.parentElement.remove();
+    clearTask();
 }
 
 function changeStatus(e) {
     var statusButton = document.querySelector('.btn.btn-success');
     statusButton.innerText = 'Incomplete';
 
-    var statusElement = statusButton.parentElement.parentElement.lastChild.previousSibling
+    var statusElement = statusButton.parentElement.parentElement.lastChild.previousSibling;
     statusElement.innerText = 'Completed'
 }
+
 function enterTask() {
     var response = prompt('Enter the Todo Item');
 
     generateTodo(1, response, Date(), 'incomplete');
+}
+
+function getWeek(week) {
+    switch (week) {
+        case 0:
+            return 'Sunday';
+            break;
+        case 1:
+            return 'Monday';
+            break;
+        case 2:
+            return 'Tuesday';
+            break;
+        case 3:
+            return 'Wednesday';
+            break;
+        case 4:
+            return 'Thrusday';
+            break;
+        case 5:
+            return 'Friday';
+            break;
+        case 6:
+            return 'Saturday';
+            break;
+    
+        default:
+            break;
+    }
 }
 
 
@@ -45,26 +94,53 @@ function getTime() {
         var date = new Date();
         var hour = date.getHours();
         var min = date.getMinutes();
-        var sec = date.getSeconds();
-        currentTime.innerText = `${hour}:${min}:${sec}`;
-
-        var day = date.getDay();
+        var sec = date.getSeconds();        
+        var week = getWeek(date.getDay());
+        var day = date.getDate();
         var month = date.getMonth();
         var year = date.getFullYear();
+        var amPm;
+        var updatedMin;
 
-        var fullDate = `${day}/${month}/${year}`;
+        if(hour <12){
+            amPm = 'AM';
+        }else{
+            amPm = 'PM';
+        }
+
+        if(hour < 12){
+            var updatedHour = hour;
+        }else{
+            var updatedHour = hour - 12; 
+        }
+        
+        if(min<10){
+            updatedMin = `0${min}`;
+        }else{
+            updatedMin = min;
+        }
+
+        if(sec<10) {
+            var updatedSec = `0${sec}`;
+        }else{
+            var updatedSec = sec;
+        }
+        
+        var time = `${updatedHour}:${updatedMin}:${updatedSec}`;
+        currentTime.innerText = time ;
+        
+        var fullDate = `${day}/${month+1}/${year} ${week} ${hour}:${updatedMin} ${amPm} ` ;
         return fullDate;
-
 }
 
-setInterval(getTime, 1000);
+
 
 function generateTodo(sn, title, date, status) {
     //JS allows the user to pass any number of args so we check if the sn is present
     // if (typeof sn === 'undefined') {
-    //     throw Error("There must be a serial number to run this behaviour")
-    // }
-    var tableRow = document.createElement('tr')
+        //     throw Error("There must be a serial number to run this behaviour")
+        // }
+    var tableRow = document.createElement('tr');
     var snDes = document.createElement('td');
     snDes.innerText = sn;
     var titleRow = document.createElement('td');
@@ -77,22 +153,22 @@ function generateTodo(sn, title, date, status) {
     var optionDelete = document.createElement('button');
     optionDelete.innerText = "Delete";
     optionDelete.classList.add("btn","btn-danger");
-
+    
     //Adding event to the button 
     optionDelete.addEventListener('click', deleteAction);
-
+    
     var optionStatus = document.createElement('button'); 
-    optionStatus.innerText = "Complete";
-    optionStatus.classList.add("btn", "btn-success");
-    optionStatus.addEventListener('click', changeStatus);
+    optionStatus.innerText = "Completed";
+    optionStatus.classList.add("btn", "btn-primary");
+    optionStatus.addEventListener('click', toggleTask);
 
     //Appending buttons to the table
     options.append(optionDelete);
     options.append(optionStatus);
     tableRow.append(snDes, titleRow, dateRow, statusRow, options);
-
+    
     return tableRow;
-
+    
 }
 
 function addToList(sn, title, date, status) {
@@ -100,12 +176,41 @@ function addToList(sn, title, date, status) {
     tableBody.append(generatedData);
 }
 
+function storeTask(initialNumber, response, getTime, status) {
+    var obj = {
+        SN : initialNumber,
+        item: response,
+        time: getTime,
+        status: status
+    }
+    
+    localStorage.setItem('obj', JSON.stringify(obj));
+}
 
-document.querySelector('#add').addEventListener('click', () => {
-    var response = prompt("Enter your todo item");    
-    var time = getTime();
-        
-    addToList(1, response, time, 'Incomplete');
-})
+function loadTask() {
+    var task = JSON.parse(localStorage.getItem('obj'));
+    console.log(task);
+    addToList(task.SN,task.item, task.time, task.status);
+}
 
+function clearTask(params) {
+    localStorage.clear();
+}
 
+function askUserAndOutput() {
+    var response = prompt("Enter your todo item");   
+    var initialNumber = 1;
+    
+    if(tableBody.lastElementChild){
+        //parsing last Serial Number field from the table
+        var lastNumber = parseInt(tableBody.lastElementChild.children[0].innerText);
+        initialNumber = lastNumber +1;
+    }
+    
+    addToList(initialNumber, response, getTime(), 'Incomplete');
+    storeTask(initialNumber, response, getTime(), 'Incomplete');
+}
+
+window.addEventListener('DOMContentLoaded', loadTask);
+add.addEventListener('click', askUserAndOutput);  
+setInterval(getTime, 1000);
