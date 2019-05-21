761c9d6308c320397347fdf8325ac663c12bca96
Shrijal <tamrakarshrijal@gmail.com>
2019-05-21 20:12:05 +0545

    display the task

--- todo/app.js
+++ todo/app.js
@@ -1,1 +1,61 @@
-const 
+const addButton = document.querySelector('#add');
+const changeButton = document.querySelector('#change');
+const clearButton = document.querySelector('#clear');
+const tableBody = document.querySelector('tbody');
+
+// Load all
+loadEventListeners();
+
+function loadEventListeners() {
+    addButton.addEventListener('click', addTask);
+}
+
+function addTask() {
+    let input = prompt('Enter The Task...');
+    let sn = getSn();
+    let date = getDate();
+    displayOnTable(sn,input,date);
+}
+
+function displayOnTable(sno,input,date) {
+
+    const tableRow = document.createElement('tr');
+
+    const snRow = document.createElement('td');
+    snRow.innerText = sno;
+
+    const dateRow = document.createElement('td');
+    dateRow.innerText = date;
+
+    const inputRow = document.createElement('td');
+    inputRow.innerText = input;
+
+    const statusRow = document.createElement('td');
+    statusRow.innerText = 'Incomplete'
+
+    const optionRow = document.createElement('td');
+
+    const toggleBtn = document.createElement('button');
+    toggleBtn.innerText = 'Complete';
+    toggleBtn.classList = 'toggle';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.classList = 'delete';
+
+    optionRow.append(toggleBtn,deleteBtn);
+
+    tableRow.append(snRow,dateRow,inputRow,statusRow,optionRow);
+
+    tableBody.append(tableRow);
+
+}
+
+function getSn() {
+    return 1;
+}
+
+function getDate() {
+    let date = new Date();
+    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+}
